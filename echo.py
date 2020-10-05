5de3fda274b1755e3e29dc8d73524fb827535ed4
Robert Havelaar <rmhavelaar@gmail.com>
2020-10-05 08:44:38 -0500

    Passes all Tests

--- echo.py
+++ echo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """An enhanced version of the 'echo' cmd line utility."""
 
-__author__ = "Robert Havelaar"
+__author__ = "Robert Havelaar with help from Jacob W."
 
 
 import sys
